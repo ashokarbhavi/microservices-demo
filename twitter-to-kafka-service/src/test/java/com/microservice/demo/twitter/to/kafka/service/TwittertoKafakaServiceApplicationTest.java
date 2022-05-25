42228067edb3d0b2977ae5e387ca2477452b33ad
Ashok <ashokaarbhavi@gmail.com>
2022-05-25 17:59:34 +0530

    initial commit

---
+++ twitter-to-kafka-service/src/test/java/com/microservice/demo/twitter/to/kafka/service/TwittertoKafakaServiceApplicationTest.java
@@ -0,0 +1,17 @@
+package com.microservice.demo.twitter.to.kafka.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootTest
+public class TwittertoKafakaServiceApplicationTest {
+	
+	@Test
+	public void contextLoad() {
+		
+	}
+}
